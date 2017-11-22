2f21bedb57c9e93b287f0cc514f059d940f2bf26
Virgorasion <fauzan.widyanto1@gmail.com>
2017-11-22 17:17:05 +0700

    Update

--- resources/views/layouts/_sidenav.blade.php
+++ resources/views/layouts/_sidenav.blade.php
@@ -1,34 +1,85 @@
 <nav id="mainnav-container">
-				<div id="mainnav">
+	<div id="mainnav">
 
-					<!--Menu-->
-					<!--================================-->
-					<div id="mainnav-menu-wrap">
-						<div class="nano">
-							<div class="nano-content">
-								<ul id="mainnav-menu" class="list-group">
+		<!--Shortcut buttons-->
+		<!--================================-->
+		<div id="mainnav-shortcut">
+		</div>
+		<!--================================-->
+		<!--End shortcut buttons-->
 
-									<!--Category name-->
-									<li class="list-header">Navigation</li>
 
-									<!--Menu list item-->
-									<li class="active-link">
-										<a href="index.html">
-											<i class="psi-home"></i>
-											<span class="menu-title">
-												<strong>Dashboard</strong>
-												<span class="label label-success pull-right">Top</span>
-											</span>
-										</a>
-									</li>
+		<!--Menu-->
+		<!--================================-->
+		<div id="mainnav-menu-wrap">
+			<div class="nano">
+				<div class="nano-content">
+					<ul id="mainnav-menu" class="list-group">
 
-									
+						<!--Category name-->
+						<li class="list-header">Navigation</li>
 
-							</div>
-						</div>
-					</div>
-					<!--================================-->
-					<!--End menu-->
+						<!--Menu list item-->
+						<li class="active-link">
+							<a href="index.html">
+								<i class="psi-home"></i>
+								<span class="menu-title">
+									<strong>Dashboard</strong>
+									<span class="label label-success pull-right">Top</span>
+								</span>
+							</a>
+						</li>
 
+						<!--Menu list item-->
+						<li>
+							<a href="index.html#">
+								<i class="psi-split-vertical-2"></i>
+								<span class="menu-title">
+									<strong>Layouts</strong>
+								</span>
+								<i class="arrow"></i>
+							</a>
+
+							<!--Submenu-->
+							<ul class="collapse">
+								<li>
+									<a href="layouts-collapsed-navigation.html">Collapsed Navigation</a>
+								</li>
+								<li>
+									<a href="layouts-offcanvas-navigation.html">Off-Canvas Navigation</a>
+								</li>
+								<li>
+									<a href="layouts-offcanvas-slide-in-navigation.html">Slide-in Navigation</a>
+								</li>
+								<li>
+									<a href="layouts-offcanvas-revealing-navigation.html">Revealing Navigation</a>
+								</li>
+								<li class="list-divider"></li>
+								<li>
+									<a href="layouts-aside-right-side.html">Aside on the right side</a>
+								</li>
+								<li>
+									<a href="layouts-aside-left-side.html">Aside on the left side</a>
+								</li>
+								<li>
+									<a href="layouts-aside-bright-theme.html">Bright aside theme</a>
+								</li>
+								<li class="list-divider"></li>
+								<li>
+									<a href="layouts-fixed-navbar.html">Fixed Navbar</a>
+								</li>
+								<li>
+									<a href="layouts-fixed-footer.html">Fixed Footer</a>
+								</li>
+
+							</ul>
+						</li>
+					</ul>
 				</div>
-			</nav>
+			</div>
+		</div>
+		<!--================================-->
+		<!--End menu-->
+
+	</div>
+</nav>
